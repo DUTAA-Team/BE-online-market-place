8571ff0ae20468c0bb9da25ff3eb06fd03ce78f6
A Samod <asamod999@gmail.com>
2024-06-10 10:59:22 +0530

    ft : user registration conpleted

---
+++ routes/userRoutes.js
@@ -0,0 +1,17 @@
+import express from 'express';
+const router = express.Router();
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+} from '../controllers/userController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
+
+router.route('/').post(registerUser).get(protect, admin, getUsers);
+
+export default router;
